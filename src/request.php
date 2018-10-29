ee62986f5f87f9641e0ddae70047a06a2e6f5af2
tomasmrazko <info@tomasmrazko.sk>
2018-10-29 12:11:12 +0100

    init

---
+++ src/request.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace foxtmtest/foxtmtest;
+
+class request{
+     
+    private $type = "POST";
+    private $timeout = 5;
+    private $apiUrl = "https://dev.api.foxentry.cz";
+    
+    function run()
+    { // BEGIN function run
+    	echo "test";
+    } // END function run
+    
+}
