159a3f61019a89b9e25842927161c79dd0f5d6aa
tomasmrazko <info@tomasmrazko.sk>
2018-10-31 09:40:13 +0100

    update

---
+++ src/base.php
@@ -0,0 +1,18 @@
+<?php
+
+class Base
+{ // BEGIN class Base
+	
+    function parseJson($json)
+    { // BEGIN function parseJson
+        $data = json_decode($json);
+        
+        if (json_last_error() == JSON_ERROR_NONE) {
+        	return $data;
+        }
+        return null;
+    } // END function parseJson
+
+} // END class Base
+
+?>
--- src/request.php
+++ src/request.php
@@ -1,16 +1,61 @@
-<?php 
-
-namespace foxtmtest/foxtmtest;
-echo "TEST";
-class request{
+<?php
+echo "test"; 
+namespace foxtmtest\foxtmtest 
+class Request{
      
-    private $type = "POST";
-    private $timeout = 5;
-    private $apiUrl = "https://dev.api.foxentry.cz";
+    private $requestOptions;
+    private $requestQuery;
+    private $endpoint;
+    
+    function __construct()
+    { // BEGIN function __construct
+    	$this->requestOptions = new stdClass();
+    	$this->requestQuery   = array();
+    } // END function __construct
+    
+    function setEndpoint($endpoint)
+    { // BEGIN function setEndpoint
+        $this->endpoint = $endpoint;	
+    } // END function setEndpoint
+    
+    function setOption($name, $value)
+    { // BEGIN function setOption
+        $this->requestOptions->$name = $value;	
+    } // END function setOption
+    
+    function setRequestCountry($country)
+    { // BEGIN function setRequestCountry
+        $this->setOption("country", $country);
+    } // END function setRequestCountry
+    
+    function setPagination($limit = 20, $offset = 0)
+    { // BEGIN function setLimits
+        $this->setOption("limits", array(
+            "results" => $limit,
+            "offset" => $offset
+        ));
+    } // END function setLimits
+    
+    function addQueryParam($data)
+    { // BEGIN function addQueryParam
+        if (!isset($this->requestQuery["params"])) {
+        	$this->requestQuery["params"] = array();
+        }  	
+        
+        $this->requestQuery["params"][] = $data;
+    } // END function addQueryParam
     
     function run()
     { // BEGIN function run
-    	echo "test";
+        print_r($this->requestOptions);
+        print_r($this->requestQuery);
+        $curl = new Curl();
+        
+        $data = array(
+            "options" => $this->requestOptions,
+            "query"   => $this->requestQuery,
+        );
+        $response = $curl->run($this->endpoint, $data);
     } // END function run
     
-}
+}      
