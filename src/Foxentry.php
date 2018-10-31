df57ef6d482d786ac39524d02e9706f233258e12
tomasmrazko <info@tomasmrazko.sk>
2018-10-31 10:48:54 +0100

    k

--- src/Foxentry.php
+++ src/Foxentry.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace Foxentry;
-
+echo "AAA";
 class Foxentry
 { // BEGIN class Foxentry
-	
+    
+    function getVersion()
+    { // BEGIN function getVersion
+    	return 1;
+    } // END function getVersion
+    	
 } // END class Foxentry
 
 ?>
